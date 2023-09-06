5e4a4423e0cbfd5396fe49f42755ebcd175b66a6
IgnaDev <arcortoon@gmail.com>
2023-09-05 23:56:23 -0300

    [SPOILER]

--- spoiler(No Entrar)/main.js
+++ spoiler(No Entrar)/main.js
@@ -3,12 +3,12 @@ const root = document.getElementById("root")
 
 //Funcion creadora de formularios
 const funcionCreadoraDeFormulario =(inputs, callback)=>{
-    const formularioContenedor = document.createElement("div")
+    const formularioContenedor = document.createElement("form")
     const botonFormulario = document.createElement("button")
 
     for (let i = 0; i < inputs.length; i++) {
         const input = document.createElement("input")
-        input.placeholder = input[i]
+        input.placeholder = inputs[i]
 
         formularioContenedor.appendChild(input)        
     }
@@ -16,7 +16,44 @@ const funcionCreadoraDeFormulario =(inputs, callback)=>{
     botonFormulario.innerText = "Enviar"
     formularioContenedor.appendChild(botonFormulario)
 
+    formularioContenedor.addEventListener("submit", callback)
+
+
     return formularioContenedor
+}
+
+//Funcion de callback
+const funcionParaElEvento = (e)=>{
+    e.preventDefault()
+    console.log("asdasd")
+    const valorNombre = e.target[0]
+    const valorCategorias = e.target[1]
+
+    if(formulario.firstChild.id == "error"){
+        formulario.removeChild(formulario.firstChild)
+    }
+    
+
+    if(valorNombre.value.length < 3) {
+        formulario.prepend(errorNodo("Nombre muy pequeño"))
+        
+        return
+    }
+    if(valorCategorias.value.length < 3) {
+        formulario.prepend(errorNodo("Categoria muy pequeño"))
+        
+        return
+    }
+
+
+    const nodoAux = funcionQueCreaNodos(valorNombre.value, valorCategorias.value)
+    nodoAux.classList.add("item-tablero")
+    
+    tablero.appendChild(nodoAux)
+
+
+    valorNombre.value = ""
+    valorCategorias.value = ""
 }
 
 
@@ -58,45 +95,16 @@ const errorNodo = (error)=>{
 
 //tablero
 const tablero = document.createElement("div")
+tablero.classList.add("tablero")
 
 
 
 
 
 //Formulario
-const formulario = funcionCreadoraDeFormulario(["Nombre", "Categoria"])
-
-formulario.addEventListener("submit",(e)=>{
-    e.preventDefault()
-    const valorNombre = e.target[0]
-    const valorCategorias = e.target[1]
-
-    if(formulario.firstChild.id == "error"){
-        formulario.removeChild(formulario.firstChild)
-    }
-    
-
-    if(valorNombre.value.length < 3) {
-        formulario.prepend(errorNodo("Nombre muy pequeño"))
-        
-        return
-    }
-    if(valorCategorias.value.length < 3) {
-        formulario.prepend(errorNodo("Categoria muy pequeño"))
-        
-        return
-    }
+const formulario = funcionCreadoraDeFormulario(["Nombre", "Categoria"], funcionParaElEvento)
 
 
-    const nodoAux = funcionQueCreaNodos(valorNombre.value, valorCategorias.value)
-    nodoAux.classList.add("tablero")
-    
-    tablero.appendChild(nodoAux)
-
-
-    valorNombre.value = ""
-    valorCategorias.value = ""
-})
 
 
 
